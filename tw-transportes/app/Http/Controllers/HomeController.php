101118152e88420ec3c2fbb666a8ff822f386bc7
Andre Luis Kunert <seuemail@example.com>
2025-08-29 20:25:57 -0300

    Bora Codar com LaravelPro_2025

--- tw-transportes/app/Http/Controllers/HomeController.php
+++ tw-transportes/app/Http/Controllers/HomeController.php
@@ -4,13 +4,13 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
-class SobreController extends Controller
+class HomeController extends Controller
 {
     /**
      * Handle the incoming request.
      */
     public function __invoke(Request $request)
     {
-        return  view('sobre');
+        return view('home');
     }
 }
--- tw-transportes/resources/views/sobre.blade.php
+++
@@ -1,2 +0,0 @@
-<h1> Conteudo Completo da Pagina de Sobre  </h1>
-<p> Primera Pagina com Laravel  </p>
